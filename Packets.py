31ca23721dfcbf6f1858a79a38196b21c7025735
IulianMurariu-Tanasache <iulian.murariutanasache@gmail.com>
2021-12-02 23:10:24 +0200

    commit

---
+++ Packets.py
@@ -0,0 +1,252 @@
+import struct
+
+from HelperFunctions import *
+import functools as fct
+
+
+class Packet:
+    def __init__(self, client):
+        self.client = client
+
+    def decode(self, data):
+        pass
+
+    def encode(self, data):
+        pass
+
+
+class ConnectPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        prot_len, protocol_name, prot_version, conn_flags, keep_alive = struct.unpack('!H4sBcH', data[0:10])
+
+        protocol_name = protocol_name.decode(encoding='utf-8')
+        # connect_flags = format(ord(conn_flags.decode(encoding='ascii')), '#010b')[2:]
+        connect_flags = bin(int.from_bytes(conn_flags, 'big')).lstrip('0b')
+
+        # print(protocol_name)
+        # print(prot_version)
+        # print(keep_alive)
+
+        if protocol_name != 'MQTT' or prot_version != 4:
+            print('Eroare conectare1')
+            return
+
+        reserved = connect_flags[7] == '1'
+        print(reserved, connect_flags[0], connect_flags, connect_flags[1])
+
+        if reserved:
+            print('Eroare conectare2')
+            return
+
+        clean_start = connect_flags[6] == '1'
+        # TODO: implementare sesiune, will message, QoS, willRetain, password, username, keep alive, session expiry, user prop?, auth method?, auth data?
+
+        self.client.will = connect_flags[5] == '1'
+        self.client.willQoS = int(connect_flags[3:5], 2)
+        self.client.willRetain = connect_flags[2] == '1'
+        password = connect_flags[1] == '1'
+        username = connect_flags[0] == '1'
+        self.client.keepAlive = keep_alive
+
+        # chestii de versiune 5.0
+        # var_length, property_length = decodeVariableInt(data[10:])
+        # properties_data = data[10 + var_length: 10 + var_length + property_length]
+        # properties = struct.unpack('!', properties_data)
+
+        payload_data = data[10:]
+        id_len, self.client.id = decodeUTF8(payload_data)
+        payload_data = payload_data[2 + id_len:]
+
+        if self.client.will:
+            wt_len, self.client.willTopic = decodeUTF8(payload_data[0:])
+            msg_len = struct.unpack('!H', payload_data[2 + wt_len:2 + wt_len + 2])[0]
+            msg = struct.unpack(f'!{msg_len}s', payload_data[wt_len + 2 + 2:wt_len + 2 + 2 + msg_len])[0]
+            payload_data = payload_data[wt_len + 2 + 2 + msg_len:]
+            print(msg)
+
+        # de implementat user si parola si restul
+        if username:
+            user_len, self.client.user = decodeUTF8(payload_data[0:])
+            self.client.user = self.client.user
+            print(self.client.user)
+            payload_data = payload_data[user_len + 2:]
+
+        if password:
+            pass_len, self.client.password = decodeUTF8(payload_data[0:])
+            self.client.password = self.client.password
+            print(self.client.password)
+            payload_data = payload_data[pass_len + 2:]
+
+        # if keep_alive:
+        #     keep_len, self.client.keepAlive = decodeUTF8(payload_data[0:])
+        #     self.client.keepAlive = self.client.keepAlive
+        #     print(self.client.keepAlive)
+        #     payload_data = payload_data[keep_len + 2:]
+
+        self.client.connected = True
+
+
+class ConnackPacket(Packet):
+    # de revenit pentru sesiune si raspuns negativ
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        fixedHeader = struct.pack('!BB', 32, 2)
+
+        # sp daca are deja sesiune sau nu
+        varHeader = struct.pack('!BB', 0, 0)
+
+        header = b''.join([fixedHeader, varHeader])
+        return header
+
+
+class SubscribePacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        packet_id, = struct.unpack('!H', data[0:2])
+        payload_data = data[2:]
+        # pachet_id e pentru a nu prelucra acelasi pachet de prea multe ori la QoS
+
+        # if data is gol -> protocol violation
+        payload_len = len(payload_data)
+        curr = 0
+
+        while curr < payload_len:
+            topic_len, topic = decodeUTF8(payload_data[curr:])
+            curr += 2 + topic_len
+            qos_byte = format(int.from_bytes(payload_data[curr:curr + 1], 'big'), '#010b')[2:]
+            good = fct.reduce(lambda a, b: a and (b == '0'), qos_byte[0:6])
+            print(good)
+            # if good == false -> malformed packet
+            qos = int(qos_byte[6:], base=2)
+
+            # if qos > 2 -> malformed packet
+            curr += 1
+
+            self.client.topics.append((topic, qos))
+            print(topic, qos)
+
+
+class PublishPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        remaining_length = len(data)
+        payload_data = data[0:]
+        len_topic, topic_name = decodeUTF8(payload_data)
+
+        start_payload = len_topic + 2
+
+        #Packet.packet_indentifer = struct.unpack('!H', data[5:7])
+
+        msgpayload = data[start_payload:remaining_length]
+        msgpayload = msgpayload.decode(encoding='utf-8')#struct.unpack(f'!{len_payload}s', msgpayload)
+        print(topic_name, msgpayload)
+
+#cumva pe interfata
+
+
+class PubackPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        fixHeader = struct.pack('!BB', 64, 2)
+
+        varheader = struct.pack('!H', data)#nu se stie ce aere
+
+        header = b''.join([fixHeader, varheader])
+        return header
+
+
+class PubrecPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        fixHeader = struct.pack('!BB', 80, 2)
+
+        varheader = struct.pack('!BB', 0, 0)#idnet variable?
+
+        header = b''.join([fixHeader, varheader])
+        return header
+
+
+class PubrelPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data): #asta era dee decodat
+        fixHeader = struct.pack('!BB', 98, 2)
+
+        varheader = struct.pack('!BB', 0, 0)  # idnet variable?
+
+        header = b''.join([fixHeader, varheader])
+        return header
+
+
+class PubcompPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        fixHeader = struct.pack('!BB', 112, 2)
+
+        varheader = struct.pack('!BB', 0, 0)  # idnet variable?
+
+        header = b''.join([fixHeader, varheader])
+        return header
+
+
+class UnsubscribePacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        pass
+
+
+class SubackPacket(Packet): #encode
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        pass
+
+
+class UnSubackPacket(Packet): # encode
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        pass
+
+
+class PingReqPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        pass
+
+
+class PingRespPacket(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def encode(self, data):
+        pass
+
+class Disconnect(Packet):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def decode(self, data):
+        pass
---
+++ Server.py
@@ -0,0 +1,128 @@
+import select
+import socket
+import threading
+import traceback
+from Packets import *
+
+
+class Session:
+    def __init__(self):
+        pass
+
+
+class Client:
+    def __init__(self, conn, addr):
+        self.conn = conn
+        self.addr = addr
+        self.connected = False
+        self.keepAlive = 0
+        self.will = False
+        self.willQoS = 0
+        self.willRetain = False
+        self.user = None
+        self.password = None
+        self.id = None
+        self.willTopic = ''
+        self.topics = []
+
+    def fileno(self):
+        return self.conn.fileno()
+
+
+class Server:
+    def __init__(self, logBox, logs):
+        self.state = False
+        self.logBox = logBox
+        self.logs = logs
+        self.port = 1883
+        self.ip = '127.0.0.1'
+        self.socket = None
+        self.clients = []
+
+    def printLog(self, log):
+        self.logs.insert(0, log)
+        self.logBox.delete(1, 'end')
+        self.logBox.insert(1, log)
+
+    def listen(self):
+        self.socket.listen()
+        while self.state:
+            try:
+                # Asteapta cereri de conectare, apel blocant
+                conn, addr = self.socket.accept()
+                new_client = Client(conn, addr)
+                self.printLog(f'Connected client with address: {new_client.addr}')
+                # mai intai astept packetul de connect, apoi setez ca e connected
+                # new_client.setConnected(True)
+                self.clients.append(new_client)
+            except OSError:
+                print('Closed')
+                break
+            except Exception as e:
+                traceback.print_exc()
+                self.printLog("Eroare la pornirea thread‐ului")
+
+    def start(self):
+        if self.state:
+            return
+        self.state = True
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port))
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.printLog('Server starting on ip: ' + self.ip)
+        self.handleClientsThread = threading.Thread(target=self.handle_clients, args=())
+        self.listenThread = threading.Thread(target=self.listen, args=())
+        self.listenThread.start()
+        self.handleClientsThread.start()
+
+    def stop(self):
+        if not self.state:
+            return
+        self.state = False
+        self.socket.close()
+        self.listenThread.join()
+        self.handleClientsThread.join()
+        self.printLog('Stopping server...')
+
+    def handleClient(self, data, client, packet_type):
+        # aici if/switch
+        if packet_type == 'CONNECT':
+            ConnectPacket(client).decode(data[0:])
+            connack = ConnackPacket(client)
+            connackData = connack.encode(None)
+            client.conn.sendall(connackData)
+
+        if packet_type == 'SUBSCRIBE':
+            SubscribePacket(client).decode(data[0:])
+
+        if packet_type == 'PUBLISH':
+            publish = PublishPacket(client)
+            publish.decode(data[0:])
+
+            # puback = PubackPacket(client)
+            # pubackData = puback.encode(publish.packet_indentifer)
+            # client.conn.sendall(pubackData)
+        if packet_type == 'DISCONNECT':
+            client.conn = None  # am inchis conexiune? si acum Will mesage?
+
+    def handle_clients(self):
+        while self.state:
+            if len(self.clients) == 0:
+                continue
+            to_read, _, _ = select.select(self.clients, [], [], 1)
+            for client in to_read:
+                self.printLog(f'{client.addr} a trimis un pachet')
+                data = client.conn.recv(1024)
+
+                while len(data) > 0:
+                    binary = format(int.from_bytes(data[0:1], "big"), '#010b')[2:]
+                    packet_type = int(binary[0:4], 2)
+                    packet_type = packet_types_to_string[packet_type]
+                    flags = binary[4:]
+                    print(packet_type)
+
+                    var_length, remaining_length = decodeVariableInt(data[1:])
+                    curr_pack = data[1 + var_length: 1 + var_length + remaining_length]
+                    data = data[1 + var_length + remaining_length:]
+                    #print(remaining_length, len(curr_pack), len(data[0:]))
+                    self.handleClient(curr_pack, client, packet_type)
